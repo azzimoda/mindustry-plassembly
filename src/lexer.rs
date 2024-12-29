d4b7155d7186e401d33f62b4457049a8627b70fc
Konstantin Matsa <konstantinmaca@gmail.com>
2024-12-29 17:20:23 +0500

    Add generic identifiers and labels. Add support for nested code blocks.
    Minor change: Fix wrong arguments of command `op` in the macro `dec` in `expample1.plas`.

--- src/lexer.rs
+++ src/lexer.rs
@@ -5,7 +5,27 @@ pub struct Lexer<'a> {
     line: usize,
 }
 
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub enum Token {
+    Number(String),
+    String(String),
+    Identifier(String),
+    Label(String),
+    MacroDef(String),
+    MacroExpand(String),
+    MacroDefEnd,
+    MacroExpandLabel(String),
+    BlockParam(String),
+    Keyword(String),
+    GenericIdentifier(String),
+    GenericLabel(String),
+    Unknown(String),
+}
+
 impl<'a> Lexer<'a> {
+    const TOKEN_RE: &'static str =
+        r#"!!|-?\d+(\.\d+)?|\$(begin|end|include)|#\w+:?|[&@!]\w+|\w+!:|\w+[:!]?|"[^"]*?""#;
+
     pub fn new(input: &'a str) -> Self {
         let lines: Vec<&str> = input
             .lines()
@@ -15,7 +35,7 @@ impl<'a> Lexer<'a> {
         Self { lines, line: 0 }
     }
 
-    pub fn all_lines(&mut self) -> Vec<Vec<String>> {
+    pub fn all_lines(&mut self) -> Vec<Vec<Token>> {
         let mut lines = Vec::new();
         while self.line < self.lines.len() {
             lines.push(self.next_line());
@@ -23,21 +43,69 @@ impl<'a> Lexer<'a> {
         lines.into_iter().filter(|e| !e.is_empty()).collect()
     }
 
-    fn next_line(&mut self) -> Vec<String> {
-        let token_re =
-            Regex::new(r#"!!|-?\d+\.\d+|-?\d+|\$(begin|end)|[&@!]\w+|\w+!:|\w+[:!]?|"[^"]*?""#)
-                .unwrap();
-        let tokens: Vec<String> = token_re
+    fn next_line(&mut self) -> Vec<Token> {
+        let token_re = Regex::new(Self::TOKEN_RE).unwrap();
+        let tokens: Vec<Token> = token_re
             .find_iter(self.lines[self.line])
             .map(|m| {
+                // if Regex::new(r#" "" "#).unwrap().is_match(&s) {
+                //     dbg!(&s);
+                // }
                 let s = m.as_str().to_string();
-                if Regex::new(r#" "" "#).unwrap().is_match(&s) {
-                    dbg!(&s);
+                if Regex::new(r"!!").unwrap().is_match(&s) {
+                    Token::MacroDefEnd
+                } else if Regex::new(r"^-?\d+(\.\d+)?$").unwrap().is_match(&s) {
+                    Token::Number(s)
+                } else if Regex::new(r#"^"[^"]*?"$"#).unwrap().is_match(&s) {
+                    Token::String(s)
+                } else if Regex::new(r"^\w+$").unwrap().is_match(&s) {
+                    Token::Identifier(s)
+                } else if Regex::new(r"^\w+:$").unwrap().is_match(&s) {
+                    Token::Label(s.strip_suffix(':').unwrap().to_string())
+                } else if Regex::new(r"^!\w+$").unwrap().is_match(&s) {
+                    Token::MacroDef(s.strip_prefix('!').unwrap().to_string())
+                } else if Regex::new(r"^\w+!$").unwrap().is_match(&s) {
+                    Token::MacroExpand(s.strip_suffix('!').unwrap().to_string())
+                } else if Regex::new(r"^\w+!:$").unwrap().is_match(&s) {
+                    Token::MacroExpandLabel(s.strip_suffix("!:").unwrap().to_string())
+                } else if Regex::new(r"^\$\w+$").unwrap().is_match(&s) {
+                    Token::Keyword(s.strip_prefix('$').unwrap().to_string())
+                } else if Regex::new(r"^&\w+$").unwrap().is_match(&s) {
+                    Token::BlockParam(s.strip_prefix('&').unwrap().to_string())
+                } else if Regex::new(r"^#\w+$").unwrap().is_match(&s) {
+                    Token::GenericIdentifier(s.strip_prefix('#').unwrap().to_string())
+                } else if Regex::new(r"^#\w+:$").unwrap().is_match(&s) {
+                    Token::GenericLabel(
+                        s.strip_prefix('#')
+                            .unwrap()
+                            .strip_suffix(':')
+                            .unwrap()
+                            .to_string(),
+                    )
+                } else {
+                    Token::Unknown(s)
                 }
-                s
             })
             .collect();
         self.line += 1;
         tokens
+    }
+}
+
+impl ToString for Token {
+    fn to_string(&self) -> String {
+        match self {
+            Token::Number(val) | Token::String(val) | Token::Identifier(val) => val.clone(),
+            Token::Label(val) => format!("{val}:"),
+            Token::MacroDef(val) => format!("!{val}"),
+            Token::MacroExpand(val) => format!("{val}!"),
+            Token::MacroDefEnd => "!!".into(),
+            Token::MacroExpandLabel(val) => format!("{val}!:"),
+            Token::BlockParam(val) => format!("&{val}"),
+            Token::Keyword(val) => format!("${val}"),
+            Token::GenericIdentifier(val) => format!("#{val}"),
+            Token::GenericLabel(val) => format!("#{val}:"),
+            Token::Unknown(val) => val.clone(),
+        }
     }
 }
--- src/processor.rs
+++ src/processor.rs
@@ -1,55 +1,66 @@
 use std::collections::HashMap;
 
+use crate::lexer::Token;
+
 pub struct MacroProcessor {
-    tokens: Vec<Vec<String>>,
+    tokens: Vec<Vec<Token>>,
     macros: HashMap<String, Macro>,
+    gen_ident_count: usize,
     i: usize,
 }
 
 #[derive(Debug, Clone)]
 struct Macro {
-    args: Vec<String>,
-    body: Vec<Vec<String>>,
+    args: Vec<Token>,
+    body: Vec<Vec<Token>>,
 }
 
 impl MacroProcessor {
-    pub fn new(tokens: Vec<Vec<String>>) -> Self {
+    pub fn new(tokens: Vec<Vec<Token>>) -> Self {
         Self {
             tokens,
             macros: HashMap::new(),
+            gen_ident_count: 0,
             i: 0,
         }
     }
 
-    fn with_macros(&self, macros: &HashMap<String, Macro>) -> Self {
-        Self {
-            tokens: self.tokens.clone(),
-            macros: macros.clone(),
-            i: self.i,
-        }
+    fn with_macros(&mut self, macros: &HashMap<String, Macro>) -> &mut Self {
+        self.macros.extend(macros.clone());
+        return self;
     }
 
-    pub fn run(&mut self) -> Vec<Vec<String>> {
+    fn with_ident_count(&mut self, n: usize) -> &mut Self {
+        self.gen_ident_count = n;
+        self
+    }
+
+    pub fn run(&mut self) -> Vec<Vec<Token>> {
         let mut result = Vec::new();
         while self.i < self.tokens.len() {
             let line = self.tokens[self.i].clone();
             self.i += 1;
-            match line[0].as_str() {
-                "$begin" | "$end" => continue,
-                s if s.starts_with('!') && s.chars().all(|c| c.is_alphanumeric() || c == '!') => {
-                    self.define_macro(&line);
-                }
-                s if s.ends_with('!') && s.chars().all(|c| c.is_alphanumeric() || c == '!') => {
-                    result.extend(self.process_macro(&line));
-                }
+
+            match &line[0] {
+                Token::MacroDef(_) => self.define_macro(&line),
+                Token::MacroExpand(_) => result.extend(self.expand_macro(&line)),
+                Token::MacroDefEnd
+                | Token::MacroExpandLabel(_)
+                | Token::BlockParam(_)
+                | Token::GenericIdentifier(_)
+                | Token::GenericLabel(_) => continue,
+                Token::Keyword(kw) if matches!(kw.as_str(), "begin" | "end") => continue,
                 _ => result.push(line),
             }
         }
         result
     }
 
-    fn define_macro(&mut self, line: &[String]) {
-        let name = &line[0][1..];
+    fn define_macro(&mut self, line: &[Token]) {
+        let name = match &line[0] {
+            Token::MacroDef(name) => name.clone(),
+            _ => unreachable!("Defining macro not on Token::MacroDef"),
+        };
         let args = line[1..].to_vec();
         self.macros.insert(
             name.to_string(),
@@ -58,9 +69,9 @@ impl MacroProcessor {
                 body: Vec::new(),
             },
         );
-        while self.tokens[self.i][0] != "!!" {
+        while !matches!(self.tokens[self.i][0], Token::MacroDefEnd) {
             self.macros
-                .get_mut(name)
+                .get_mut(&name)
                 .expect("Failed to get existing macro to add an argument.")
                 .body
                 .push(self.tokens[self.i].clone());
@@ -69,44 +80,62 @@ impl MacroProcessor {
         self.i += 1;
     }
 
-    fn process_macro(&mut self, line: &[String]) -> Vec<Vec<String>> {
-        let name = &line[0][..line[0].len() - 1];
+    fn expand_macro(&mut self, line: &[Token]) -> Vec<Vec<Token>> {
+        let name = match &line[0] {
+            Token::MacroExpand(name) => name.clone(),
+            token => unreachable!("Expanding macro not on Token::MacroExpand(...): {token:?}"),
+        };
         // dbg!(&self.macros);
         let macro_def = self
             .macros
-            .get(name)
+            .get(&name)
             .unwrap_or_else(|| panic!("Macro with name '{name}' is not found."))
             .clone();
-        let args: HashMap<String, String> = macro_def
+        let args: HashMap<String, Token> = macro_def
             .args
             .iter()
             .zip(line[1..].iter())
-            .filter(|(p, _)| !p.starts_with('&'))
-            .map(|(p, a)| (p.clone(), a.clone()))
+            .filter(|(p, _)| !matches!(p, Token::BlockParam(..)))
+            .map(|(p, a)| (p.to_string(), a.clone()))
             .collect();
-        let block_args: HashMap<String, Vec<Vec<String>>> = macro_def
+        let block_args: HashMap<String, Vec<Vec<Token>>> = macro_def
             .args
             .iter()
-            .filter(|p| p.starts_with('&'))
-            .map(|p| (p.clone(), self.parse_block()))
+            .filter(|p| matches!(p, Token::BlockParam(..)))
+            .map(|p| {
+                let key = match p {
+                    Token::BlockParam(param) => param.clone(),
+                    _ => unreachable!(),
+                };
+                (key, self.parse_block())
+            })
             .collect();
 
         let mut body_with_macros = Vec::new();
+        let mut local_gen_idents: HashMap<String, String> = HashMap::new();
         for line in &macro_def.body {
             let lines = if line.len() == 1 {
                 // dbg!(&line);
-                if let Some(key) = line[0].strip_suffix("!:") {
-                    if let Some(arg) = args.get(key) {
-                        vec![vec![format!("{}:", arg)]]
+                if let Token::MacroExpandLabel(param) = &line[0] {
+                    if let Some(arg) = args.get(param) {
+                        vec![vec![Token::Label(arg.to_string())]]
                     } else {
                         vec![line.clone()]
                     }
-                } else if line[0].starts_with('&') {
+                } else if let Token::BlockParam(param) = &line[0] {
                     // dbg!(&block_args);
-                    if let Some(block) = block_args.get(&line[0]) {
+                    if let Some(block) = block_args.get(param) {
                         block.clone()
                     } else {
                         vec![line.clone()]
+                    }
+                } else if let Token::GenericLabel(local_ident) = &line[0] {
+                    if let Some(label) = local_gen_idents.get(local_ident) {
+                        vec![vec![Token::Label(label.clone())]]
+                    } else {
+                        let label = self.generate_ident(&local_ident);
+                        local_gen_idents.insert(local_ident.clone(), label.clone());
+                        vec![vec![Token::Label(label)]]
                     }
                 } else {
                     vec![line.clone()]
@@ -114,11 +143,19 @@ impl MacroProcessor {
             } else {
                 let mut new_line = Vec::new();
                 for token in line {
-                    if token.ends_with('!') {
-                        if let Some(arg) = args.get(&token[..token.len() - 1]) {
+                    if let Token::MacroExpand(param) = token {
+                        if let Some(arg) = args.get(param) {
                             new_line.push(arg.clone());
                         } else {
                             new_line.push(token.clone());
+                        }
+                    } else if let Token::GenericIdentifier(local_ident) = token {
+                        if let Some(ident) = local_gen_idents.get(local_ident) {
+                            new_line.push(Token::Identifier(ident.clone()))
+                        } else {
+                            let ident = self.generate_ident(&local_ident);
+                            local_gen_idents.insert(local_ident.clone(), ident.clone());
+                            new_line.push(Token::Identifier(ident))
                         }
                     } else {
                         new_line.push(token.clone());
@@ -131,25 +168,44 @@ impl MacroProcessor {
 
         MacroProcessor::new(body_with_macros)
             .with_macros(&self.macros)
+            .with_ident_count(self.gen_ident_count)
             .run()
     }
 
-    fn parse_block(&mut self) -> Vec<Vec<String>> {
+    fn parse_block(&mut self) -> Vec<Vec<Token>> {
         let mut block = Vec::new();
         self.i += 1;
-        while self.tokens[self.i][0] != "$end" {
+        let mut depth = 0;
+        loop {
+            match &self.tokens[self.i][0] {
+                Token::Keyword(kw) if kw == "begin" => depth += 1,
+                Token::Keyword(kw) if kw == "end" && depth == 0 => break,
+                Token::Keyword(kw) if kw == "end" => depth -= 1,
+                _ => (),
+            }
             block.push(self.tokens[self.i].clone());
             self.i += 1;
         }
         self.i += 1;
         block
     }
+
+    fn generate_ident(&mut self, s: &str) -> String {
+        let i = self.gen_ident_count;
+        self.gen_ident_count += 1;
+        format!("__GI_{i}_{s}")
+    }
 }
 
-pub fn stringify_tokens(tokens: Vec<Vec<String>>) -> String {
+pub fn stringify_tokens(tokens: Vec<Vec<Token>>) -> String {
     tokens
         .into_iter()
-        .map(|s| s.join(" "))
+        .map(|s| {
+            s.iter()
+                .map(|t| t.to_string())
+                .collect::<Vec<String>>()
+                .join(" ")
+        })
         .collect::<Vec<String>>()
         .join("\n")
 }
